684717b7f43ccc17d996126209af2736aea113bd
MarijnS95 <marijns95@gmail.com>
2016-10-17 00:27:27 +0200

    Changed some PtrToStructures to unsafe dereferencers.

--- FreePIE.Core.Plugins/VJoy/FfbPacket.cs
+++ FreePIE.Core.Plugins/VJoy/FfbPacket.cs
@@ -1,6 +1,7 @@
 ﻿using FreePIE.Core.Plugins.VJoy.PacketData;
 using System;
 using System.Runtime.InteropServices;
+using System.Text;
 
 namespace FreePIE.Core.Plugins.VJoy
 {
@@ -31,17 +32,18 @@ namespace FreePIE.Core.Plugins.VJoy
         public PacketType PacketType { get; }
         public int BlockIndex { get; }
 
-        public FfbPacket(IntPtr packetPtr)
+        public unsafe FfbPacket(IntPtr packetPtr)
         {
             //copy ffb packet to managed structure
-            packet = (InternalFfbPacket)Marshal.PtrToStructure(packetPtr, typeof(InternalFfbPacket));
+            //packet = (InternalFfbPacket)Marshal.PtrToStructure(packetPtr, typeof(InternalFfbPacket));
+            packet = *(InternalFfbPacket*)packetPtr;
 
             //A packet contains only a tiny bit of information, and a pointer to the actual FFB data which is interesting as well.   
             if (packet.DataSize < 10)
                 throw new Exception(string.Format("DataSize incorrect, {0}", packet.DataSize));
 
             //Read out the first two bytes (into the base packetData class), so we can fill out the 'important' information
-            basePacketData = GetPacketData<BasePacket>();
+            basePacketData = *(BasePacket*)packet.PtrToData; //GetPacketData<BasePacket>();
             BlockIndex = basePacketData.BlockIndex;
             DeviceId = (basePacketData.IdxAndPacketType & 0xF0) >> 4;
             if (DeviceId < 1)
@@ -51,32 +53,30 @@ namespace FreePIE.Core.Plugins.VJoy
             //DEBUG: print useful information
             Console.WriteLine("----------------------");
             Console.WriteLine("DataSize: {0}, CMD: {1}", packet.DataSize, packet.Command);
-            Console.WriteLine(Data.ToHexString());
+            Console.WriteLine(GetHexString());
             Console.WriteLine("BlockIdx: {0}", BlockIndex);
             Console.WriteLine("Device ID: {0}", DeviceId);
             Console.WriteLine("Packet type: {0}", PacketType);
         }
 
-        public byte[] Data
+        public unsafe string GetHexString()
         {
-            get
-            {
-                byte[] outBuffer = new byte[packet.DataSize];
-                Marshal.Copy(packet.PtrToData, outBuffer, 0, packet.DataSize - 8);//last 8 bytes are not interesting? (haven't seen them in use anywhere anyway)
-                return outBuffer;
-            }
+            StringBuilder sb = new StringBuilder();
+            byte* data = (byte*)packet.PtrToData;
+            for (int i = 0; i < packet.DataSize - 8; i++)
+                sb.AppendFormat("{0:X2}", data[i]);
+            return sb.ToString();
         }
 
         public T GetPacketData<T>()
         where T : IFfbPacketData
         {
+            //return *(T*)packet.PtrToData; -_- no generic pointers for you! Use the 'slow' marshaller instead!
+            //Actually, this is one of the so-disliked micro-optimalisations:
+            //Is performance bad? Well I don't know, never tested. Just want this code to run as fast as possible (little latency).
+            //If so, where's the bottleneck? I know the marshaller is slow, but the current way of asynchronously executing packets doesn't really look streamlined either.
+            //So it's probably not a big deal.
             return (T)Marshal.PtrToStructure(packet.PtrToData, typeof(T));
-        }
-
-        public void CopyPacketData<T>(T originalPacket)
-        where T : IFfbPacketData
-        {
-            Marshal.PtrToStructure(packet.PtrToData, originalPacket);
         }
     }
 }
